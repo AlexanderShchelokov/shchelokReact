14e772a5fea7f25a6e72db56dbc12291a445b047
a.schelokov <a.schelokov@mediasoft.team>
2018-08-21 15:24:41 +0400

    Added interval change

--- src/Counter.js
+++ src/Counter.js
@@ -7,11 +7,12 @@ export default class Counter extends React.Component {
 
         this.state = {
             count: 0,
-            interval: 1000
+            interval: 1000,
+            step: 1000
         }
     }
 
-    incState = () => {
+    incCount = () => {
         this.setState({
             count:  this.state.count + 1
         })          
@@ -19,14 +20,17 @@ export default class Counter extends React.Component {
 
     reset = () => {
         this.setState({
-            count: 0
+            count: 0,     
+            interval: 1000,
+            step: 1000          
         })
+        clearInterval(this.timer);
     }
 
     startCounting = () => { 
         clearInterval(this.timer); 
         this.timer = setInterval(() => { 
-            this.incState() 
+            this.incCount() 
         },  
         this.state.interval) 
     }
@@ -35,14 +39,45 @@ export default class Counter extends React.Component {
         clearInterval(this.timer);   
     }
 
+    resetInterval = () => {
+        this.stopCounting()
+        this.startCounting()
+    }
+
+    editInterval = (oper) => {
+        switch (oper) {
+            case '+':
+                this.setState({
+                    interval: this.state.interval + this.state.step
+                })
+                this.resetInterval()               
+                break;
+            case '-':
+                if (this.state.interval - this.state.step <= 0)
+                    break
+                this.setState({
+                    interval: this.state.interval - this.state.step
+                })
+                this.resetInterval()               
+                break;                
+            default:
+                break;
+        }
+    }
+
     render() {
         return (
            <div>
                 <div>{this.state.count}</div>
-                <button onClick={() => this.incState()}>Увеличить на 1</button>
+                <button onClick={() => this.incCount()}>Увеличить на 1</button>
                 <button onClick={() => this.reset()}>Сбросить</button>
                 <button onClick={() => this.startCounting()}>Запустить счетчик</button>
                 <button onClick={() => this.stopCounting()}>Остановить счетчик</button>
+                <br></br>
+                <div>Шаг {this.state.interval / 1000} секунд</div>
+                <button onClick={() => this.editInterval('+')}>Увеличить интервал на {this.state.step / 1000} секунду</button>
+                <button onClick={() => this.editInterval('-')}>Уменьшить интервал на {this.state.step / 1000} секунду</button>
+                
           </div>
         );
     }
